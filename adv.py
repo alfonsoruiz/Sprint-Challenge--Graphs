787a46ad2858b90a7fd8a7e87d87771d3e278461
alfonsoruiz <1800ruiz@gmail.com>
2020-11-29 12:00:09 -0800

    mvp

--- adv.py
+++ adv.py
@@ -5,6 +5,73 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Stack
+
+
+class Graph():
+    def __init__(self):
+        self.visited = {}
+        self.rev_path = Stack()
+        self.rev_directions = {
+            'n': 's',
+            'w': 'e',
+            'e': 'w',
+            's': 'n'
+        }
+
+    def not_visited_exits(self, id):
+        not_visited = []
+        exits = self.visited[id]
+
+        for direction, exit in exits.items():
+            if exit == '?':
+                not_visited.append(direction)
+
+        return not_visited
+
+    def add_direction(self, id, direct, dest=None):
+        if dest is None:
+            dest = '?'
+
+        self.visited[id][direct] = dest
+
+    def add_room(self, id):
+        self.visited[id] = {}
+
+    def dft(self, direct=None, prev=None):
+        if len(self.visited) == len(room_graph):
+            return
+
+        if direct is not None:
+            player.travel(direct)
+            traversal_path.append(direct)
+            self.rev_path.push(self.rev_directions[direct])
+
+        curr_room = player.current_room
+
+        if curr_room.id not in self.visited:
+            self.add_room(curr_room.id)
+            exits = curr_room.get_exits()
+
+            for exit in exits:
+                self.add_direction(curr_room.id, exit)
+
+        if prev is not None:
+            self.add_direction(prev.id, direct, curr_room.id)
+            self.add_direction(
+                curr_room.id, self.rev_directions[direct], prev.id)
+
+        non_visited_exits = self.not_visited_exits(curr_room.id)
+
+        if len(non_visited_exits) > 0:
+            for exit in non_visited_exits:
+                self.dft(exit, curr_room)
+
+        rev_step = self.rev_path.pop()
+        player.travel(rev_step)
+        traversal_path.append(rev_step)
+
+
 # Load world
 world = World()
 
@@ -17,7 +84,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -28,7 +95,8 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
+world_graph = Graph()
+world_graph.dft()
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,11 +109,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
